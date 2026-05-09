2c8e92133bcfc3a4172479334da5a932eb345ae1
sksingh3882-cmyk <sksingh3882@gmail.com>
2026-05-09 18:37:29 +0530

    Connect hamburger menu to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import AppMenu from "./components/AppMenu";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AppMenu />
+        {children}
+      </body>
     </html>
   )
 }
