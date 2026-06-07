9be366705b252bfe8a799e1a9ec0ada2dd4d4426
sksingh3882-cmyk <sksingh3882@gmail.com>
2026-06-07 18:17:58 +0530

    Add mock assignment data

---
+++ app/admin/driver-vehicle-assignment/_lib/mockAssignmentData.ts
@@ -0,0 +1,20 @@
+import type { AssignmentBookingDetails } from "./assignmentTypes";
+
+export function getMockAssignmentData(
+  bookingId: string
+): AssignmentBookingDetails {
+  return {
+    bookingId,
+    customerName: "Test Customer",
+    customerMobile: "98xxxxxx12",
+    serviceType: "Outstation / Local / Airport",
+    date: "10 June 2026",
+    time: "08:00 AM",
+    pickupArea: "Sonari",
+    dropArea: "Ranchi Airport",
+    pickupFullAddress: "Sonari, Jamshedpur",
+    dropFullAddress: "Ranchi Airport",
+    vehicleType: "Sedan",
+    vehicleModel: "Dzire / Etios / Similar",
+  };
+}
