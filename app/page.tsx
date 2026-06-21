417b96d1e5028afabf23a7c8cdb6aa38e7560a31
sksingh3882-cmyk <sksingh3882@gmail.com>
2026-06-21 18:31:20 +0530

    Increase height of textHeroFeatures

--- app/page.tsx
+++ app/page.tsx
@@ -147,7 +147,7 @@ const textHeroOrange:CSSProperties={
 
 const textHeroFeatures:CSSProperties={
   marginTop:14,
-  height:92,
+  height:118,
   padding:"8px 10px",
   backgroundImage:`url("/cars/premium-strip.png")`,
   backgroundSize:"cover",
